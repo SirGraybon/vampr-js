1eb80bfa1483521cb4f70caaa302b6d8194268fc
SirGraybon <graydonritchie@gmail.com>
2023-04-21 18:42:49 -0400

    added new features

--- vampire.js
+++ vampire.js
@@ -10,22 +10,32 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let fromSource = 0;
+    let currentVamp = this;
+    while (currentVamp.creator) {
+      currentVamp = currentVamp.creator;
+      fromSource++;
+    }
+    return fromSource;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (this.numberOfVampiresFromOriginal > vampire.numberOfVampiresFromOriginal) {
+      return false;
+    }
+    return true;
   }
 
   /** Stretch **/
@@ -36,7 +46,35 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    let thisLine = [];
+    let thatLine = [];
+    let currentVamp = this;
+    let otherCurrentVamp = vampire;
+    while (currentVamp.creator) {
+      thisLine.push(currentVamp);
+      currentVamp = currentVamp.creator;
+    }
+    while (otherCurrentVamp.creator) {
+      thatLine.push(otherCurrentVamp);
+      otherCurrentVamp = otherCurrentVamp.creator;
+    }
+    for (const vamp of thisLine) {
+      for (const compVamp of thatLine) {
+        if (vamp === compVamp) {
+          return vamp
+        }
+      }
+    }
+return currentVamp
+  }
 
+  totalDecendants() {
+    let decendants = 0
+    decendants +=1 
+    for (const decendant of this.decendants) {
+      decendants += decendant.totalDecendants()
+    }
+    return decendants
   }
 }
 
